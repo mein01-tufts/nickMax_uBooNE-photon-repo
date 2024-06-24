ac6e73e59e395490890d808fe673e82d04a9b7ff
Maxwell <maxwell.ein@tuftsledu>
2024-06-24 10:22:40 -0400

    edited some comments

--- binSorter.py
+++ binSorter.py
@@ -52,7 +52,7 @@ protonGammaHist = configureHist(protonGammaHist)
 pionGammaHist = configureHist(pionGammaHist)
 protonPionGammaHist = configureHist(protonPionGammaHist)
 
-#Set detector volumes
+#Set detector min/max and fiducial width (cm)
 xMin, xMax = 0, 256
 yMin, yMax = -116.5, 116.5
 zMin, zMax = 0, 1036
@@ -64,10 +64,11 @@ for i in range(eventTree.GetEntries()):
 
   eventTree.GetEntry(i)
 
-  #filter for neutral curren
+  #filter for neutral current events
   if eventTree.trueNuCCNC != 1:
     continue
 
+#fiducial cut removes everything within fiducialWidth of detector wall
   if eventTree.trueVtxX <= (xMin + fiducialWidth) or eventTree.trueVtxX >= (xMax - fiducialWidth) or \
     eventTree.trueVtxY <= (yMin + fiducialWidth) or eventTree.trueVtxY >= (yMax - fiducialWidth) or \
     eventTree.trueVtxZ <= (zMin + fiducialWidth) or eventTree.trueVtxZ >= (zMax - fiducialWidth):
