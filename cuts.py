da4e7017c7848cc3a73b52f7d35620de3cf6928d
Nick W. Dahlen <ndahle01@login-prod-01.pax.tufts.edu>
2024-06-25 15:44:56 -0400

    Added an umbrella function to cuts that filters for signal using truth

--- cuts.py
+++ cuts.py
@@ -1,5 +1,11 @@
 #NOTES:
 #All of these functions are meant to operate within a larger function that loops through every event (or the chosen events) within the eventTree
+import sys, argparse
+import numpy as np
+import ROOT as rt
+
+from helpers.larflowreco_ana_funcs import getCosThetaGravVector
+
 
 def trueCutNCC(eventTree):
 #Filter for neutral current using truth - False if CC, True if NC
@@ -97,5 +103,92 @@ def trueCutEDep(eventTree):
       break
   if badPhoton == True:
     return False
+  else:
+    return True
+
+
+
+
+
+
+
+  
+def trueSingalFinder(eventTree):
+#Uses a series of truth-based checks to determine signal as thoroughly as possible
+  xMin, xMax = 0, 256
+  yMin, yMax = -116.5, 116.5
+  zMin, zMax = 0, 1036
+  fiducialWidth = 10
+
+  badEvent = False
+
+  if eventTree.trueNuCCNC != 1:
+    badEvent = True
+
+#fiducial cut removes everything within fiducial
+
+  if eventTree.trueVtxX <= (xMin + fiducialWidth) or eventTree.trueVtxX >= (xMax - fiducialWidth) or \
+    eventTree.trueVtxY <= (yMin + fiducialWidth) or eventTree.trueVtxY >= (yMax - fiducialWidth) or \
+    eventTree.trueVtxZ <= (zMin + fiducialWidth) or eventTree.trueVtxZ >= (zMax - fiducialWidth):
+    badEvent = True
+        
+  #skip events where all hits overlap with tagged cosmic rays
+  if eventTree.vtxFracHitsOnCosmic >= 1.:
+    badEvent = True
+
+  photonInSecondary = False
+  primList = []
+  photonIDList = []
+  protonPresent = False
+  PionPresent = False
+
+  #Determining presence of suitably energetic protons and pions
+  for x in range(len(eventTree.truePrimPartPDG)):
+    if eventTree.truePrimPartPDG[x] == 211:
+      pionEnergy = eventTree.truePrimPartE[x] - np.sqrt(eventTree.truePrimPartE[x]**2 - (eventTree.truePrimPartPx[x]**2+eventTree.truePrimPartPy[x]**2+eventTree.truePrimPartPz[x]**2))
+      if pionEnergy >= 0.03:
+        pionPresent = True
+        badEvent = True
+    elif eventTree.truePrimPartPDG[x] == 2212:
+      protonEnergy = eventTree.truePrimPartE[x] - np.sqrt(eventTree.truePrimPartE[x]**2 - (eventTree.truePrimPartPx[x]**2+eventTree.truePrimPartPy[x]**2+eventTree.truePrimPartPz[x]**2))
+      if protonEnergy >= 0.06:
+        protonPresent = True
+        badEvent = True
+  
+  #Check if Neutral Pion and Kaon in primaries
+  if 111 in eventTree.truePrimPartPDG or 311 in eventTree.truePrimPartPDG:
+    #Check if there is actually a detectable photon
+    for x in range(eventTree.nTrueSimParts):
+      if eventTree.trueSimPartPDG[x] == 22:
+        photonIDList.append(x)
+        photonInSecondary = True
+  else:
+  #Create a list of prime particle Track IDs
+    for x in range(len(eventTree.trueSimPartTID)):
+      if eventTree.trueSimPartTID[x] == eventTree.trueSimPartMID[x]:
+        primList.append(eventTree.trueSimPartTID[x])
+    #Iterate through to find photons
+    for x in range(len(eventTree.trueSimPartPDG)):
+      if eventTree.trueSimPartPDG[x] == 22:
+        photonIDList.append(x)
+        #Check for parent particle in the primary list
+        if eventTree.trueSimPartMID[x] in primList:
+          photonInSecondary = True      
+        #Failing that, check if the photon has coordinates within 15 mm of those of the event vertex
+        elif abs(eventTree.trueSimPartX[x] - eventTree.trueVtxX) <= 0.15 and abs(eventTree.trueSimPartY[x] - eventTree.trueVtxY) <= 0.15 and abs(eventTree.trueSimPartZ[x] -eventTree.trueVtxZ) <= 0.15:
+          photonInSecondary = True
+  #Discard event unless a secondary photon is found
+  if photonInSecondary == False:
+    badEvent = True  
+
+    #Discard event unless the photon begins to deposit energy within the fiducial volume
+  for x in range(len(photonIDList)):
+    if eventTree.trueSimPartEDepX[x] <= (xMin + fiducialWidth) or eventTree.trueSimPartEDepX[x] >= (xMax - fiducialWidth) or eventTree.trueSimPartEDepY[x] <= (yMin + fiducialWidth) or eventTree.trueSimPartEDepY[x] >= (yMax - fiducialWidth) or eventTree.trueSimPartEDepZ[x] <= (zMin + fiducialWidth) or eventTree.trueSimPartEDepZ[x] >= (zMax - fiducialWidth):
+      badEvent = True
+
+
+  if badEvent == True:
+    return False
+  
   else:
     return True
