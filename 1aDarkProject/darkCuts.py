976121fabce43a2104d70dd0722adfe23daedfea
Maxwell <maxwell.ein@tuftsledu>
2024-11-14 13:14:13 -0500

    added directory to store dark matter research files

---
+++ 1aDarkProject/darkCuts.py
@@ -0,0 +1,13 @@
+# Welcome to darkCuts: It's like cuts.py, but like..... dark
+
+# Some imported libraries we may need
+import sys, argparse
+import numpy as np
+import ROOT as rt
+import math
+
+# Placeholder
+def cut(eventTree):
+    for i in range(len(eventTree)):
+        print("hello world" + str(i))
+    return True
